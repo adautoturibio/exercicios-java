1cb06749e9f47833ac1f08a1c1de2d4913ac58d3
AdautoTuribio <adautoturibio73@gmail.com>
2022-11-16 00:57:57 -0300

    melhoria

--- exercicio1.12.23/src/Main.java
+++ exercicio1.12.23/src/Main.java
@@ -1,13 +1,16 @@
 public class Main {
     public static void main(String[] args) {
         
-        double s, q=1.5, n=50, a1=1/1; 
-        double potencia = Math.pow(q,n);
+        double s=1/1;
         
-        s = (a1*(potencia-1))/q-1; // FORMULA DE SOMA DOS TERMOS DE UMA PG FINITA
-
-        System.out.println("S = "+s);        
+        for (int i=1; i<51; i++){
+            s = s*1.5;
+        }
+        
+        System.out.printf("Soma = ",+s);
+        System.out.print(s);
     }   
+
 }      
 
           
